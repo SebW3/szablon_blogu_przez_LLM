94a4632225eabe2807528eb0c2087d8144aa2bf4
SebW3 <sebwoss@gmail.com>
2024-11-15 16:34:37 +0100

    Zadanie dodatkowe
    Generowanie szablonu HTML wraz z CS i JS

---
+++ wygeneruj_szablon.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+from openAI_api_key import openAI_api_key
+
+
+with open("artykul.html", "r", encoding="utf-8") as file:
+    artykul = file.read()
+
+def stworz_szablon(artykul):
+    client = OpenAI(api_key=openAI_api_key())
+    print("Tworzenie szablonu...")
+    prompt_system = "Twoim zadaniem jest stworzenie szablonu HTML dla podanego artykułu. " \
+                    "Stwórz CSS i prosty JS. Podpisy obrazów muszą się znajdować pod obrazami. " \
+                    "Sekcja body ma być pusta (tam będzie artykuł). Stwórz navbar. " \
+                    "Wszystkie elementy mają się znajdować w 1 pliku. Zwróć sam kod"
+    messages = [{"role": "system", "content": prompt_system}, {"role": "user", "content": artykul}]
+
+    response = client.chat.completions.create(messages=messages, model="gpt-4o-mini", temperature=0)
+
+    artykul = response.choices[0].message.content
+
+    return artykul.replace("```html", "").replace("```", "")
+
+
+
+szablon = stworz_szablon(artykul)
+
+with open("szablon.html", "w", encoding="utf-8") as file:
+    file.write(szablon)
